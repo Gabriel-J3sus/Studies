89f582e941e5defe1f556efc29f5b99e9a9cbdb1
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-12-09 12:08:47 -0300

    Animated Form

---
+++ animated-form/03/script.js
@@ -0,0 +1,8 @@
+const btnLogin = document.querySelector('.btn-login');
+const form = document.querySelector("form");
+
+btnLogin.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    form.classList.add('form-hide');
+});
