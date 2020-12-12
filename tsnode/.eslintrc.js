47e56036ac2860ba50dd67921b3320c452fde739
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-12-12 16:34:30 -0300

    TSNode

---
+++ tsnode/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
