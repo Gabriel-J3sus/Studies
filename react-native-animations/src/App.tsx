7ce8aedd27b04970e0ed9bf516d5f86256efa8a2
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-01-05 16:32:20 -0300

    Animations

---
+++ react-native-animations/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Scroll from './pages/Scroll';
+import  Login from './pages/login';
+import Drag from './pages/Drag';
+
+export default function App() {
+  return (
+    // <Scroll />
+    // <Login />
+    <Drag />
+  );
+}
