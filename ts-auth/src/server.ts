70b2bceddb2494ad7f791b82bb7bb572ef6f9dd0
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-01-08 15:37:35 -0300

    ts-auth

---
+++ ts-auth/src/server.ts
@@ -0,0 +1,12 @@
+import express, { response } from 'express';
+
+import './database/connection'; //connection
+import routes from './routes'; //routes file
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333); // http://localhost:3333
