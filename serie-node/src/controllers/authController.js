fafbefb7840a832542d14385cd2d484b179c894b
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-01-16 13:07:22 -0300

    serie-node

--- serie-node/src/controllers/authController.js
+++ serie-node/src/controllers/authController.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user');
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, { expiresIn: '1d' });
+}
 
 router.post('/register', async (req, res) => {
   const { email } = req.body;
@@ -16,11 +24,36 @@ router.post('/register', async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
 
   } catch (error) {
     return res.status(400).send({ error: 'Registration fails' });
   }
 });
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return res.status(400).send({ error: 'User not found' });
+  }
+
+  if (!await bcrypt.compare(password, user.password)) {
+    return res.status(400).send({ error: 'Invalid password' });
+  }
+
+  user.password = undefined;
+
+  res.send({
+    user,
+    token: generateToken({ id: user.id }),
+  });
+})
+
 
 module.exports = app => app.use('/auth', router);
--- serie-node/src/index.js
+++ serie-node/src/index.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json()); //know when I'm sending data in json type
 app.use(bodyParser.urlencoded({ extended: false })); //know when Im ending parameters by url
 
 require('./controllers/authController')(app)
+require('./controllers/projectController')(app)
 
 app.listen(3333);
