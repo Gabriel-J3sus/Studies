db549b82740e56742d31d4582217d22e0759e036
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-01-27 21:00:21 -0300

    skylabNext

--- skylabnext/contexts/theme/ThemeContainer.tsx
+++ skylabnext/contexts/theme/ThemeContainer.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { ColorModeProvider, CSSReset, ThemeProvider as ChakraThemeProvider } from '@chakra-ui/react';
+import { ColorModeProvider, ChakraProvider } from '@chakra-ui/react';
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 
 import theme from '../../styles/theme';
 
 const ThemeContainer: React.FC = ({ children }) => {
     return (
-        <ChakraThemeProvider theme={theme}>
-            <ColorModeProvider options={{ initialColorMode: 'dark' }}>
-                <EmotionThemeProvider theme={theme}>
-                    <CSSReset />
-                    {children}
-                </EmotionThemeProvider>
+        <ChakraProvider theme={theme} resetCSS={true}>
+            <ColorModeProvider value='dark' options={{ initialColorMode: 'dark' }}>
+                    {children}    
             </ColorModeProvider>
-        </ChakraThemeProvider>
+        </ChakraProvider>
     );
 }
 
