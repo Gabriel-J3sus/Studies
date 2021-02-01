28c775a2e4bcbfd7f9c5818d39e7f277ff8102ed
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-02-01 13:18:20 -0300

    chat

---
+++ chat/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);//protocolo http
+const io = require('socket.io')(server); //protocolo wss
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+  res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', socket => {
+  console.log(`conectado: ${socket.id}`)
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data);
+    socket.broadcast.emit('receivedMessage', data)
+  })
+})
+
+server.listen(3000)
