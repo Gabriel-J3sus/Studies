cd97e56807ca6b2cb682d223ae5cb5e720ae5782
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-03-03 14:40:33 -0300

    nextjs basic

---
+++ nextjs-basic/pages/cars/[id].js
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+export default function Car({ car }) {
+    const router = useRouter() // allow to use the query params
+    const { id } = router.query;
+
+    return (
+        <>
+            <Head>
+                <title>{car.color} {car.id}</title>
+            </Head>
+
+            <h1>Hello {id}</h1>
+            <img src={car.image} width="300px" />
+        </>
+    )
+}
+
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data }
+    }
+}
+
+// export async function getStaticProps({ params }) {
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data }
+//     }
+// }
+
+// export async function getStaticPaths() {
+//     const req = await fetch(`http://localhost:3000/cars.json`);
+//     const data = await req.json();
+
+//     const paths = data.map(car => {
+//         return { params: { id: car } }
+//     })
+
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
---
+++ nextjs-basic/pages/cars/index.js
@@ -0,0 +1,3 @@
+export default function CarsList() {
+    return <h1>CarsList</h1>
+}
