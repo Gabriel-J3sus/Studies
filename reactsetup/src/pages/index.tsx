46a14ab4049e55b7ff9015eaaa13150b7e087c8a
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-03-16 17:20:28 -0300

    reactsetup

---
+++ reactsetup/src/pages/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-use-before-define */
+import React from 'react'
+import Head from 'next/head'
+
+import RocketseatLogo from '../assets/rocketseat.svg'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <RocketseatLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+    </Container>
+  )
+}
+
+export default Home
